3491c97c9d3edfbbd45d92c6ec5b1a29b8114077
XboxSignOut <152027553+XboxSignOut@users.noreply.github.com>
2024-10-06 01:01:16 +0530

    cretaed routes for user schema to interact with the database

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const router = require("./routes/user-routes");
 
 const app = express();
+
+app.use('/api', router);
 
 mongoose.connect("mongodb+srv://vaibhavi062024:LZw8cv3UXeZyhKMx@cluster0.9hmba.mongodb.net/auth?retryWrites=true&w=majority&appName=Cluster0").then(() => {
   app.listen(5000);
