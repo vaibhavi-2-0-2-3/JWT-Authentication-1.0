7b998d8d170ecfc78191949de35cc0ae97ed55a2
XboxSignOut <v.a.i.bhavi062024@gmail.com>
2024-10-06 13:42:10 +0530

    Getting user details

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,63 +1,100 @@
-const User = require ('../model/User');
-const bcrypt = require("bcryptjs");
+const User = require('../model/User');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const JWT_SECRET_KEY = "MyKey";
 
-const signup = async(req, res, next) => {
-  const {name, email, password} = req.body;
+const signup = async (req, res, next) => {
+  const { name, email, password } = req.body;
   let existingUser;
-  try{
-    existingUser = await User.findOne({email: email});
-  }catch(err){
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
     console.log(err);
   }
 
-  if(existingUser) {
-    return res.status(400).json({message: "user already exists! Login instead"});
+  if (existingUser) {
+    return res.status(400).json({ message: "User already exists! Login instead" });
   }
 
   const hashedPassword = bcrypt.hashSync(password);
 
   const user = new User({
-    name, // name: name
+    name,
     email,
     password: hashedPassword,
   });
 
-  try{
+  try {
     await user.save();
-  } catch(err) {
+  } catch (err) {
     console.log(err);
   }
 
-  return res.status(201).json({message:user});
+  return res.status(201).json({ message: user });
 };
 
-const login = async(req,res, next) => {
-  const {email, password } = req.body;
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
 
-  let exisitingUser;
-  try{
-    exisitingUser = await User.findOne({email: email});
-
-  }catch(err) {
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
     return new Error(err);
   }
-  if(!exisitingUser){
-    return res.status(400).json({message:"User not found, signup Please"})
+  if (!existingUser) {
+    return res.status(400).json({ message: "User not found, please signup" });
   }
-  const isPasswordCorrect = bcrypt.compareSync(password, exisitingUser.password);
-  if(!isPasswordCorrect){
-    return res.status(400).json({message:'Invalid Email / Password'})
+  const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+  if (!isPasswordCorrect) {
+    return res.status(400).json({ message: 'Invalid Email / Password' });
   }
 
-  const token = jwt.sign({id: exisitingUser._id}, JWT_SECRET_KEY, {
-    expiresIn: "1hr"
+  const token = jwt.sign({ id: existingUser._id }, JWT_SECRET_KEY, {
+    expiresIn: "1h"
   });
 
-  return res.status(200).json({message:'Successfully Logged In', user:exisitingUser,token});
+  return res.status(200).json({ message: 'Successfully Logged In', user: existingUser, token });
+};
 
-}
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+
+  if (!authHeader) {
+    return res.status(404).json({ message: "No token found" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  
+  if (!token) {
+    return res.status(404).json({ message: "Token missing in the header" });
+  }
+
+  jwt.verify(String(token), JWT_SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.status(400).json({ message: "Invalid Token" });
+    }
+
+    req.id = user.id;
+    next();
+  });
+};
+
+const getUser = async (req, res, next) => {
+  const userId = req.id;
+  let user;
+  try {
+    user = await User.findById(userId, "-password");  // Corrected from 'user.findById' to 'User.findById'
+  } catch (err) {
+    return new Error(err);
+  }
+  if (!user) {
+    return res.status(404).json({ message: "User Not Found" });
+  }
+  return res.status(200).json({ user });
+};
 
 exports.signup = signup;
 exports.login = login;
+exports.verifyToken = verifyToken;
+exports.getUser = getUser;
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const {signup, login} = require('../controllers/user-controller');
+const {signup, login, verifyToken, getUser} = require('../controllers/user-controller');
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.get("/user", verifyToken, getUser);
 module.exports = router;
