9ec4e00dd2fbeeb2ff2f1414ab10693c8ee257f7
XboxSignOut <v.a.i.bhavi062024@gmail.com>
2024-10-07 10:27:40 +0530

    Sent Signup http post request

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,46 +1,70 @@
 import { Box, Button, TextField, Typography } from '@mui/material';
 import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function Signup() {
+  const history = useNavigate();
   const [inputs, setInputs] = useState({
     name: "",
     email: "",
-    password: ""
-  })
+    password: "",
+  });
+
   const handleChange = (e) => {
-    e.preventDefault();
+    setInputs((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const sendRequest = async () => {
+    const res = await axios.post('http://localhost:5000/api/signup', {
+      name: inputs.name,
+      email: inputs.email,
+      password: inputs.password
+    }).catch(err => console.log(err));
+    const data = await res.data;
+    return data;
   }
 
   const handleSubmit = (e) => {
-    e.preventDefault(); // Prevent form from reloading the page
-    // Handle signup logic here
+    e.preventDefault(); // Prevent form from reloading
+    // Send http request
+    sendRequest().then(()=>history("/login"));
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <Box marginLeft="auto" marginRight="auto" width={300} display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-          <Typography value={inputs.name} variant='h2'>Signup</Typography>
-          <TextField name='name'
-          onChange={handleChange}
-          value={inputs.name}
-          variant="outlined" 
-          placeholder="Name" 
-          margin='normal' />
-          <TextField name='email'
-          onChange={handleChange}
-          type={"email"}
-          value={inputs.email} 
-          variant="outlined" 
-          placeholder="Email" 
-          margin='normal' />
-          <TextField name='password'
-          onChange={handleChange}
-          value={inputs.password}
-          variant="outlined" 
-          placeholder="Password" 
-          margin='normal' 
-          type="password" />
+          <Typography variant='h2'>Signup</Typography>
+          <TextField
+            name="name"
+            onChange={handleChange}
+            value={inputs.name}
+            variant="outlined"
+            placeholder="Name"
+            margin='normal'
+          />
+          <TextField
+            name="email"
+            onChange={handleChange}
+            type="email"
+            value={inputs.email}
+            variant="outlined"
+            placeholder="Email"
+            margin='normal'
+          />
+          <TextField
+            name="password"
+            onChange={handleChange}
+            value={inputs.password}
+            variant="outlined"
+            placeholder="Password"
+            margin='normal'
+            type="password"
+          />
           <Button type="submit" variant="contained">Signup</Button>
         </Box>
       </form>
